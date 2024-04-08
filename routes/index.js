4b0408b070826b14c7b48db41f3f99a9f838dc4d
Jose Galvez-Hernandez <josemgalvezh90@gmail.com>
2024-04-07 19:25:57 -0700

    wip: rename index ejs and route update

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'PLANTIE' });
+  res.render('home', { title: 'PLANTIE' });
 });
 
 router.get('/auth/google', passport.authenticate(
